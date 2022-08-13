5bd8f89ed339db57dc0d503e0ef6b87f4325f294
Yoni Widhi Cahyadi <yoni@mit.id>
2022-08-13 10:04:07 +0700

    Update: Venida static config & env setup

--- config/config.ts
+++ config/config.ts
@@ -4,3 +4,42 @@
  * SEJE - Digital
  */
 
+ namespace Config {
+
+    export class Config {
+
+        private Env = Venida.import('dotenv', true);
+
+        private readonly config: {[key: string]: any} = {};
+
+        /**
+         * Constructor Method
+         */
+        constructor () {
+
+            this.Env.config();
+
+            this.config = {
+
+                server: {
+                    name: process.env.APP_NAME ?? 'Venida Platform',
+                    host: process.env.SERVER_HOST ?? '127.0.0.1',
+                    port: process.env.SERVER_PORT ?? 8080,
+                    environment: process.env.APP_MODE ?? 'development'
+                },
+
+                controller: 'App.Http.Controllers',
+                model: 'App.Models',
+                hooks: 'App.Http.Hooks',
+                applicationPath: '/app',
+
+            }
+        }
+
+        public get (key: string) {
+            return this.config[key];
+        }
+    }
+}
+
+module.exports = new Config.Config();
--- config/database.ts
+++ config/database.ts
@@ -4,3 +4,58 @@
  * SEJE - Digital
  */
 
+ namespace Config {
+
+    export class Database {
+
+        private Env = Venida.import('dotenv', true);
+
+        private readonly config: {[key: string]: any} = {};
+
+        /**
+         * Constructor Method
+         */
+        constructor () {
+
+            this.Env.config();
+
+            this.config = {
+
+                default: 'mysql',
+
+                connection: {
+
+                    mysql: {
+                        client: 'mysql2',
+                        connection: {
+                            host : process.env.DB_HOST ?? '127.0.0.1',
+                            port : process.env.DB_PORT ?? 3306,
+                            user : process.env.DB_USER ?? 'root',
+                            password : process.env.DB_PASSWORD ?? '',
+                            database : process.env.DB_NAME ?? 'venida_platform'
+                          }                        
+                    },
+
+                    postgresql: {
+                        client: 'pg',
+                        version: '7.x',
+                        connection: {
+                            host : process.env.DB_HOST ?? '127.0.0.1',
+                            port : process.env.DB_PORT ?? 3306,
+                            user : process.env.DB_USER ?? 'root',
+                            password : process.env.DB_PASSWORD ?? '',
+                            database : process.env.DB_NAME ?? 'venida_platform'
+                        }
+                    }
+                }
+
+            }
+        }
+
+        public get (key: string) {
+            return this.config[key];
+        }
+    }
+}
+
+module.exports = new Config.Database();
