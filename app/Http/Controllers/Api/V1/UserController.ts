e62bcf86f2f081ec53ab92e42b8bb0792daf8b58
Yoni Widhi Cahyadi <yoni@mit.id>
2022-09-11 08:58:47 +0700

    Update: Passing Route to Controller

---
+++ app/Http/Controllers/Api/V1/UserController.ts
@@ -0,0 +1,20 @@
+/**
+ * Venida Platform
+ * Free Open Source Software
+ * SEJE - Digital
+ */
+
+ namespace App.Http.Controllers.Api.V1 {
+
+    export class UserController {
+
+
+        public async index () {
+
+            console.log('Called UserController');
+        }
+
+    }
+ }
+
+ module.exports = App.Http.Controllers.Api.V1.UserController;
--- system/Routes/Api.ts
+++ system/Routes/Api.ts
@@ -6,6 +6,8 @@
 
 namespace System.Routes.API {
 
+    const RegExpMatch = Venida.import('path-to-regexp', true).match;
+
     module.exports = (router: any, option: any, next: any) => {
 
         /**
@@ -43,11 +45,49 @@ namespace System.Routes.API {
                 let params = separator.concat(Venida.Request.get(req, 'params')['*']).replace('/index', '');
 
                 let pathParams = params.split(separator)[1];
+                let routeData   = route?.alias.split(separator);
 
-                let aliasParams = pathParams;
+                console.log('pathParams',pathParams);
+                console.log('route.query', route?.query);
 
-                console.log('pathParams✅', pathParams);
-                
+                let config = {
+                    method: method.toUpperCase(),
+                    params: [],
+                    func: routeData[2] ? routeData[2] : 'index',
+                    version: version
+                }
+
+                if (pathParams && !route?.query) {
+                    console.error('Request Not Implement');
+                }
+
+                if (pathParams && route?.query) {
+
+                    let match = RegExpMatch(route.query, { decode: decodeURIComponent });
+
+                    let check = match(pathParams);
+
+                    config['params'] = Object.values(check?.params);
+                }
+
+                /**
+                 * Require Http Controller
+                 */
+                let HttpController = Venida.import(
+                    'Venida.'
+                    .concat(route?.controller)
+                );
+
+                let Controller = new HttpController(req, res);
+
+                if (Controller && typeof Controller[config?.func] == 'function') {
+
+                    await Controller[config?.func].apply(Controller, option?.params);
+                } else {
+
+                    console.error('Controller not implements');
+                }
+
 
             } else {
                 /**
